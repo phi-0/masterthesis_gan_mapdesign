0051cb1363e334479a4e5b36d1fecf71c3cc13d0
Pascal Himmelberger <pascal.himmelberger@gmail.com>
2020-12-17 16:31:30 +0100

    VM setup shell script added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
-FROM tensorflow/tensorflow:nightly-gpu-py3-jupyter
-#FROM tensorflow/tensorflow:2.3.1-gpu-jupyter-python
+FROM tensorflow/tensorflow:2.3.1-gpu-jupyter
 
 RUN apt-get update
 
@@ -11,7 +10,7 @@ RUN git clone https://github.com/phi-0/masterthesis_gan_mapdesign.git
 
 RUN apt-get install -y python3.8 python3-pip
 
-RUN export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+#RUN export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
 
-RUN export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+#RUN export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 #RUN pip3 install -r ./masterthesis_gan_mapdesign/requirements.txt
---
+++ vmsetup.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+echo Setting up data mount...
+echo --------------------------------------
+mkdir /data
+mount -t ext3 /dev/vdb1 /data
+
+echo Installing Docker...
+echo --------------------------------------
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo Check fingerprint: $(apt-key fingerprint 0EBFCD88)
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo Installing NVIDIA container toolkit...
+echo --------------------------------------
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \ 
+&& curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-docker2
+
+echo Restarting docker runtime
+systemctl restart docker
+
+echo Checking NVIDIA docker installation
+docker run --rm --gpus all nvidia/cuda:10.0-base nvidia-smi
