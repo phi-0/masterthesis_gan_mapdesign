a09715ba3c6ff46981620556885fcdc432fb7cfd
Pascal Himmelberger <pascal.himmelberger@gmail.com>
2021-03-05 16:39:21 +0100

    S3 output image upload shell script finalised

--- scripts/output_s3_upload.sh
+++ scripts/output_s3_upload.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
-# uploads a selected number of images while choosing a random number between 1-10 to select a single image per group
+#!/bin/bash
+# RUN WITH BASH - uploads a selected number of images while choosing a random number between 1-10 to select a single image per group
 
 for imagenum in $@
 do
-    rnd=$((1 + ${RANDOM} % 10))
+    rnd=$((1 + $RANDOM % 10))
     echo "uploading sample_image_epoch$imagenum-$rnd.png --> s3://storage_hil/output/sample$imagenum.png"
-    sudo s3cmd put /data/output/images/uploading sample_image_epoch$imagenum-$rnd.png.png s3://storage_hil/output/sample$imagenum.png
+    sudo s3cmd put /data/output/images/sample_image_epoch$imagenum-$rnd.png s3://storage_hil/output/sample$imagenum.png
 done
