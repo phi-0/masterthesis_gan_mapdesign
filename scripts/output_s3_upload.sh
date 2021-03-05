f0e374b43c24b83498ef4bd1a37a91485afd7aa8
Pascal Himmelberger <pascal.himmelberger@gmail.com>
2021-03-05 16:24:03 +0100

    update shell script

--- scripts/output_s3_upload.sh
+++ scripts/output_s3_upload.sh
@@ -3,7 +3,7 @@
 
 for imagenum in $@
 do
-    rnd=$((1 + $RANDOM % 10))
+    rnd=$((1 + ${RANDOM} % 10))
     echo "uploading sample_image_epoch$imagenum-$rnd.png --> s3://storage_hil/output/sample$imagenum.png"
     sudo s3cmd put /data/output/images/uploading sample_image_epoch$imagenum-$rnd.png.png s3://storage_hil/output/sample$imagenum.png
 done
