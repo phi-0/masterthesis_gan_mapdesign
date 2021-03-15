300f5410f41328dac7528e623eb57ee204b121a5
Ubuntu <ubuntu@pascal-2t4.novalocal>
2021-03-15 16:11:32 +0100

    random crop linux version added

---
+++ scripts/image_preprocessing/random_crop_linux.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# refer to https://stackoverflow.com/questions/57221754/how-to-autocrop-randomly-using-pil for the basis of this logic
+
+from random import randrange
+from PIL import Image
+from os import walk, path
+
+################ SETTINGS ################
+folderpath = r"/data/input/maps"
+outpath = r"/data/input/crops"
+
+# cropping settings
+dim = 1024      #target dimension (symmetrical dim x dim matrix)
+n = 10          #number of samples to be taken per input image
+##########################################
+
+
+#get file list generator with os.walk()
+for _, _, files in walk(folderpath):
+    f_count = 1
+
+    for file in files:
+        print(f'File {str(i)} of {str(len(files))}')
+        #only continue with PNG files ( filtering out .ZIP files)
+        if file[-3:len(file)] == 'png':
+
+            #print(f'Working on image: {file}')
+            #loop over images in directory
+            img = Image.open(path.join(folderpath, file))
+            x, y = img.size
+            name = img.filename.replace('.png','') #get filename and remove filetype extension
+
+            #loop over samples per input image
+
+            samples = []
+            for s in range(n):
+                x1 = randrange(0, x - dim)
+                y1 = randrange(0, y - dim)
+                samples.append(img.crop((x1, y1, x1 + dim, y1 + dim)))
+
+            # save output images when NOT mostly black
+            for i, sample in enumerate(samples):
+                #get color range
+                colors = sample.getcolors() # this method returns None if the number of colors exceeds the default value of 256.
+
+                #with the following condition we filter out mostly black / unicolor images which don't hold any information
+                if colors == None or len(colors) > 2:
+                    sample.save(f"{outpath}/{file.replace('.png','')}_crop{str(i)}.png", 'PNG')
+                else:
+                    pass
+
+	f_count += 1
+
